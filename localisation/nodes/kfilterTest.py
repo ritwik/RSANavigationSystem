2ae2f7757b7d27769a2517a878401ae7506c0e79
COMP3431 11s2 - Aditya Keswani <z3242379@hal.cse.unsw.EDU.AU>
2011-10-11 22:19:57 +1100

    Adding path planner

--- localisation/nodes/kfilterTest.py
+++ localisation/nodes/kfilterTest.py
@@ -1,70 +1,119 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('localisation')
+import rospy
 
 from numpy import *
 from math import *
 
+from localisation.msg import State
+from beaconfinder.msg import Beacons, Beacon
+from std_msgs.msg import Header
+
+#Actual values
+BX = [-1.0, 0.0, -1.0]
+BY = [2.0, 3.0, -2.0]
+
+pub = rospy.Publisher('State', State)
+
+#x, y, theta
 mean = array([[0.0], [0.0], [0.0]])
 covar = array([[25000.0, 0.0, 0.0], [0.0, 25000.0, 0.0], [0.0, 0.0, 25000.0]])
 
-def observationUpdate(dx, dy, distance, angle):
+def publishState():
+    global pub
+    global mean
+    global covar
+    #Chuck mean and covar into state message and publish
+    pub.publish(State(Header(), mean[0, 0], mean[1, 0], mean[2, 0], covar[0, 0], covar[1, 1], covar[2, 2]))
+
+def actionUpdate(move):
+    #Set up necessary matrices
+    F = array([[1, 0, -move.fwd * sin(theta)],
+               [0, 1, move.fwd * cos(theta)],
+               [0, 0, 1]])
+
+    B = array([[cos(theta), 0],
+               [sin(theta), 0],
+               [0, 1]])
+
+    R = array([[cos(theta), -sin(theta), 0]
+               [sin(theta), cos(theta), 0],
+               [0, 0, 1]])
+    E = array([[0.1 + 0.01 * abs(move.fwd), 0, 0],
+               [0, 0.1 + 0.01 * abs(move.fwd), 0],
+               [0, 0, 5 * 180 / pi + 0.1 * abs(move.phi)]]) #Should these be squared?!
+    Q = dot(dot(R, E), transpose(R))
+
+    #Chuck action into matrix too
+    u = array([[move.fwd, move.phi]])
+
+    #Plug into formulae to get new mean and covariance
+    mean = dot(dot(F, mean) + B, u)
+    covar = dot(dot(F, covar), transpose(F)) + Q
+
+    #Chuck mean and covar into state message and publish
+    publishState()
+    
+def observationUpdate(data):
     global mean
     global covar
 
-    #Set up necessary matrices
-    #Something tells me this is very wrong atm
-    H = array([[0, 0, 0],
-               [-dx / sqrt(dx ** 2 + dy ** 2), -dy / sqrt(dx ** 2 + dy ** 2), 0],
-               [dy / (dx ** 2 + dy ** 2), -dx / (dx ** 2 + dy ** 2), -1]])
+    for b in data.beacon:
+        print b.ID, b.distance, b.angle
 
-    print "H = " + str(H)
+        x = mean[0, 0]
+        yy = mean[1, 0]
 
-    print "Hx = " + str(dot(H, mean))
+        dx = BX[b.ID] - x
+        dy = BY[b.ID] - yy
 
-    R = array([[1, 1, 1],
-               [0, 0.03 * distance, 0],
-               [0, 0, 0.1 * abs(angle)]]) #Should these be squared?!
+        #Set up necessary matrices
+        #Something tells me this is very wrong atm
+        H = array([[0, 0, 0],
+                   [-dx / sqrt(dx ** 2 + dy ** 2), -dy / sqrt(dx ** 2 + dy ** 2), 0],
+                   [dy / (dx ** 2 + dy ** 2), -dx / (dx ** 2 + dy ** 2), -1]])
 
-    print "R = " + str(R)
+        print "H = " + str(H)
 
-    #Chuck beacon observation into matrix
-    z = array([[0], [distance], [angle]])
+        print "Hx = " + str(dot(H, mean))
 
-    print "z = " + str(z)
-    print angle * 180 / pi
+        R = array([[1, 1, 1],
+                   [0, 0.1 * b.distance, 0],
+                   [0, 0, 0.1 * abs(b.angle)]]) #Should these be squared?!
 
-    #Plug into formulae to get new mean and covariance
-    y = z - dot(H, mean)
+        print "R = " + str(R)
 
-    print "y = " + str(y)
+        #Chuck beacon observation into matrix
+        z = array([[0], [b.distance], [b.angle]])
 
-    S = dot(dot(H, covar), transpose(H)) + R
+        print "z = " + str(z)
+        print b.angle * 180 / pi
 
-    print "S = " + str(S)
+        #Plug into formulae to get new mean and covariance
+        y = z - dot(H, mean)
 
-    K = dot(dot(covar, transpose(H)), linalg.inv(S))
+        print "y = " + str(y)
 
-    print "K = " + str(K)
+        S = dot(dot(H, covar), transpose(H)) + R
+    
+        print "S = " + str(S)
 
-    mean = mean + dot(K, y)
+        K = dot(dot(covar, transpose(H)), linalg.inv(S))
 
-    print "mean = " + str(mean)
+        print "K = " + str(K)
 
-    covar = dot((eye(3) - dot(K, H)), covar)
+        mean = mean + dot(K, y)
 
-    print "covar = " + str(covar)
+        print "mean = " + str(mean)
 
-for i in range(0, 10):
-    #This works...or not...
-    #observationUpdate(1.0, 2.0, 5 ** 0.5, atan2(2.0, 1.0))
-    #observationUpdate(1.0, -2.0, 5 ** 0.5, atan2(-2.0, 1.0))
+        covar = dot((eye(3) - dot(K, H)), covar)
 
-    #observationUpdate(-1.0, 2.0, 5 ** 0.5, atan2(2.0, -1.0))
-    #observationUpdate(-1.0, -2.0, 5 ** 0.5, atan2(-2.0, -1.0))
+        print "covar = " + str(covar)
 
-    #This is converging to pi???
-    #It's possible, not too likely, but possible
-    #dx, dy, dist, angle
-    observationUpdate(0.0, 2.0, 2.0, pi / 2)
-    observationUpdate(0.0, -2.0, 2.0, -pi / 2)
+b0 = Beacon(0, 0.0, 0.0, 5 ** 0.5, atan2(2, -1))
+b2 = Beacon(2, 0.0, 0.0, 5 ** 0.5, atan2(-2, -1))
+msg = Beacons(Header(), 2, [b0, b2])
 
-    #observationUpdate(-1.0, 0.0, 1.0, 
+for i in range(0,50):
+    observationUpdate(msg)
+
---
+++ pathplanner/nodes/pathplanner.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+
+import math
+
+#Algo
+#Construct a tree
+#Breadth first search
+#Have a FIFO queue of nodes to search
+#Once you reach the leaves (no more to search) store the value
+#Repeat for all
+#Find minimum
+
+FORWARD_SPEED = 
+TURN_SPEED = 
+
+pQueue = []
+fullPaths = []
+
+class Node:
+    def __init__(self, x, y, heading, remaining, path, time):
+        self.x = x
+        self.y = y
+        self.heading = heading
+
+        self.remaining = remaining
+        self.path = path
+
+        self.time = self.time
+
+class Coordinate:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class TimesAndHeadings:
+    def __init__(self, fwdTime, fwdHeading, bwdTime, bwdHeading):
+        self.fwdTime = fwdTime
+        self.fwdHeading = fwdHeading
+        self.bwdTime = bwdTime
+        self.bwdHeading = bwdHeading
+
+def newTimesAndHeadings(fromNode, toCoordinate):
+    dy = fromNode.y - toCoordinate.y
+    dx = fromNode.x - toCoordinate.x
+
+    distance = math.sqrt(dx** 2 + dy ** 2)
+    newHeading = atan2(dy, dx)
+    
+    #4 options: turn positive or negative, move backwards or forwards
+    #Positive forwards, positive backwards, negative forwards, negative backwards
+    angleBetween = newHeading - fromNode.heading
+
+    if (angleBetween > math.pi):
+        fwdTurnAmount = 2 * math.pi - angleBetween
+    else:
+        fwdTurnAmount = angleBetween
+    fwdHeading = newHeading
+
+    if (angle ):
+        bwdTurnAmount = 
+    else:
+        bwdTurnAmount = 
+    bwdHeading = newHeading + math.pi?
+
+    turnOptions = [
+        (newHeading - fromNode.heading)),
+        (2 * math.pi - (newHeading - fromNode.heading)),
+    ]
+
+        (pi - (newHeading - fromNode.heading))
+        (pi + (newHeading - fromNode.heading)),
+    ]
+
+    fwdTime = distance / FORWARD_SPEED + fwdTurnAmount / TURN_SPEED
+    bwdTime = distance / FORWARD_SPEED + bwdTurnAmount / TURN_SPEED
+
+    return TimesAndHeadings(fwdTime, fwdHeading, bwdTime, bwdHeading)
+
+def findPath(startNode):
+    pQueue.append(startNode)
+
+    while len(pQueue) != 0:
+        currNode = pQueue.dequeue()
+        if (currNode.remaining.isEmpty()):
+            fullPaths.append(currNode)
+        else:
+            for unexplored in currNode.remaining:
+                timesAndHeadings = newTimesAndHeadings(currNode, unexplored)
+                
+                pQueue.add(Node(unexplored.x, unexplored.y, timesAndHeadings.fwdHeading, 
+                                [n for n in currNode.remaining if n != unexplored], 
+                                currNode.path.add(currNode), timesAndHeadings.fwdTime)
+                pQueue.add(Node(unexplored.x, unexplored.y, timesAndHeadings.bwdHeading, 
+                                [n for n in currNode.remaining if n != unexplored], 
+                                currNode.path.add(currNode), timesAndHeadings.bwdTime)
+            
+def chooseBestPath():
+    pub.publish(new Path())
+
+def __main__():
+    #Open file, get nodes
+    findPath(Node(x, y, heading, [#Add all points]))
+    sendBestPath()
