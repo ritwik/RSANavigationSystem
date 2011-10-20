3537fbfb7fc01cb484350ce38b2975b8631e6548
Charley Peng <cpeng92@gmail.com>
2011-10-21 01:30:55 +1100

    I should write a script to fix up the thing

--- rosStage.sh
+++ rosStage.sh
@@ -4,6 +4,6 @@ gnome-terminal --command="bash -c \"roscore; exec bash\"" &
 
 gnome-terminal --command="bash -c \"rosrun beaconfinder beaconfinder.py\"" &
 gnome-terminal --command="bash -c \"rosrun localisation kfilter.py\"" &
-gnome-terminal --command="bash -c \"rosrun `rospack find control` beacons.world\"" &
+gnome-terminal --command="bash -c \"rosrun stage stageros `rospack find control`/beacons.world\"" &
 gnome-terminal --command="bash -c \"rosrun control control.py\"" &
 
